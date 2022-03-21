0ea04d6f40f6da5c61c0eed33f62096d33cd4401
facundo <facuwilli@gmail.com>
2022-03-21 10:31:15 -0300

    Added math debugger.

---
+++ Algebra/Assets/vector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using MathDebbuger;
+
+public class vector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
